83f7475d51117a1267f99b310f914be2dda81228
Marty Fay <marty.r.fay@gmail.com>
2023-06-01 12:51:26 -0300

    Complete functionality of game.

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,67 @@
 let game = (function () {
   let currentPlayer = "X";
-  events.on("playerPlay", changeCurrentPlayer);
+  let roundCount = 0;
+  let gameMarks = ["X", "O"];
+  let winnerBar = document.querySelector(".winner-bar");
 
+  // subscribe to events
+  events.on("playerPlayed", changeCurrentPlayer);
+  events.on("playerPlayed", checkIfWinner);
+  events.on("restart", restart);
+
+  function restart() {
+    currentPlayer = "X";
+    roundCount = 0;
+    events.emit("changedPlayer", currentPlayer);
+  }
   function changeCurrentPlayer() {
     currentPlayer === "X" ? (currentPlayer = "O") : (currentPlayer = "X");
+    incrementRounds();
+    events.emit("changedPlayer", currentPlayer);
   }
-  return {
-    currentPlayer,
-  };
+  function incrementRounds() {
+    roundCount++;
+    console.log(roundCount);
+  }
+  function checkIfWinner(currentArray) {
+    if (roundCount < 5) return;
+    for (gameMark of gameMarks) {
+      let indices = getAllIndexes(currentArray, gameMark);
+      if (winnerIndexPatternChecker(indices)) {
+        return endGame(gameMark);
+      }
+    }
+    if (roundCount === 9) return endGame("tie");
+  }
+  function getAllIndexes(arr, val) {
+    var indexes = [],
+      i;
+    for (i = 0; i < arr.length; i++) if (arr[i] === val) indexes.push(i);
+    return indexes;
+  }
+  function winnerIndexPatternChecker(indices) {
+    const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2],
+    ];
+    for (let winningCondition of winningConditions) {
+      if (winningCondition.every((i) => indices.includes(i))) {
+        return true;
+      }
+    }
+    return false;
+  }
+  function endGame(gameMark) {
+    if (gameMark === "tie") {
+      return (winnerBar.textContent = "Tie game");
+    }
+    winnerBar.textContent = `${gameMark} wins!`;
+    events.emit("gameOver");
+  }
 })();
--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -1,21 +1,50 @@
 let gameboard = (function () {
   let boardBoxes = document.querySelectorAll(".board-box");
-
-  // initialize game array to an array of empty strings of length 9
+  let currentPlayerH1 = document.querySelector(".current-player");
+  let restartButton = document.querySelector(".restart");
+  let currentPlayer = "X";
   let gameArray = [];
   for (let i = 0; i < 9; i++) gameArray.push("");
 
-  render();
+  // set up pubsub events.
+  events.on("changedPlayer", render);
+  events.on("gameOver", disableAllBoxes);
+
+  // add event listeners
   boardBoxes.forEach((box) => box.addEventListener("click", addToArray));
-  function render() {
+  restartButton.addEventListener("click", restart);
+
+  render();
+
+  function restart() {
+    gameArray = [];
+    for (let i = 0; i < 9; i++) gameArray.push("");
+    render();
+    boardBoxes.forEach((box) => box.addEventListener("click", addToArray));
+    events.emit("restart");
+  }
+  function render(player) {
+    if (player) {
+      currentPlayerH1.textContent = `Current player: ${player}`;
+      currentPlayer = player;
+    }
     boardBoxes.forEach(
       (box) => (box.textContent = gameArray[parseInt(box.id) - 1])
     );
   }
   function addToArray() {
-    gameArray[this.id - 1] = game.currentPlayer;
-    events.emit("playerPlayed");
+    gameArray[this.id - 1] = currentPlayer;
     render();
+    this.removeEventListener("click", addToArray);
+    this.classList.remove("playable");
+    events.emit("playerPlayed", gameArray);
+  }
+
+  function disableAllBoxes() {
+    boardBoxes.forEach((box) => {
+      box.classList.remove("playable");
+      box.removeEventListener("click", addToArray);
+    });
   }
 
   function getCurrentArray() {
